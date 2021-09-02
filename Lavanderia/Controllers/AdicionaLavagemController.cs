9be776bc9eb1523beef20cf8d985faeefab6c70d
marcelog5 <marcelovictorg2@gmail.com>
2021-09-02 20:34:11 -0300

    Adição da estrutura inicial do projeto
    Co-authored-by: Washington Bispo <juniorbispo515@gmail.com>

---
+++ Lavanderia/Controllers/AdicionaLavagemController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lavanderia.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdicionaLavagemController : ControllerBase
+    {
+        [HttpPost]
+        public void Post()
+        {
+            
+        }
+    }
+}
