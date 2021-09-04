8ee235edbc18321968317bcaa08a1afee21e765e
marcelog5 <marcelovictorg2@gmail.com>
2021-09-04 10:49:04 -0300

    Adição da string de conexão do MySql
    Co-authored-by: Washington Bispo <juniorbispo515@gmail.com>

---
+++ Lavanderia/Config/DBConfig/DBConection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lavanderia.Config.DBConfig
+{
+    public static class DBConection
+    {
+        public static string Conexao = "Server=MySQL;Database=lavanderia;User Id=root;Password=password;";
+    }
+}
