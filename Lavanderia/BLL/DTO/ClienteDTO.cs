86d3ab9a9fe12f0c7bab8f4e3ee5887cf069560d
marcelog5 <marcelovictorg2@gmail.com>
2021-09-04 10:47:28 -0300

    Adição das DTOs Cliente e Roupas
    Co-authored-by: Washington Bispo <juniorbispo515@gmail.com>

---
+++ Lavanderia/BLL/DTO/ClienteDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lavanderia.BLL.DTO
+{
+    public class ClienteDTO
+    {
+        [Key]
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string email { get; set; }
+
+        public string nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string cpf { get; set; }
+    }
+}
---
+++ Lavanderia/BLL/DTO/RoupasDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lavanderia.BLL.DTO
+{
+    public class RoupasDTO
+    {
+        [Key]
+        public int RoupasId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public int quantidadeRoupas { get; set; }
+
+        public DateTime horaEntrada { get; set; }
+        
+        public ClienteDTO cliente { get; set; }
+    }
+}
