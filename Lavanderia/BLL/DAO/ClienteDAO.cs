0ba64033cdd14e1adeff6a907dfb173e4fba7cb7
marcelog5 <marcelovictorg2@gmail.com>
2021-09-04 10:48:09 -0300

    Adição das DAO de inserção de cliente e roupa

---
+++ Lavanderia/BLL/DAO/ClienteDAO.cs
@@ -0,0 +1,24 @@
+﻿using Lavanderia.BLL.DTO;
+using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Lavanderia.Config.DBConfig;
+
+namespace Lavanderia.BLL.DAO
+{
+    public class ClienteDAO
+    {
+        //Inserir um cliente!
+        public void InserirCliente(ClienteDTO cliente)
+        {
+            using (var conn = new MySqlConnection(DBConection.Conexao))
+            {
+                conn.Open();
+                conn.Insert<ClienteDTO>(cliente);
+            }
+        }
+    }
+}
---
+++ Lavanderia/BLL/DAO/RoupasDAO.cs
@@ -0,0 +1,24 @@
+﻿using Dapper.Contrib.Extensions;
+using Lavanderia.BLL.DTO;
+using Lavanderia.Config.DBConfig;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lavanderia.BLL.DAO
+{
+    public class RoupasDAO
+    {
+        //Inserir um cliente!
+        public void InserirRoupa(RoupasDTO cliente)
+        {
+            using (var conn = new MySqlConnection(DBConection.Conexao))
+            {
+                conn.Open();
+                conn.Insert<RoupasDTO>(cliente);
+            }
+        }
+    }
+}
