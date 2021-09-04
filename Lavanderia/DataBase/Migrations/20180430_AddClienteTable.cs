99a59a06b4f6cefa8aa1b7131cc5bac9427437bd
marcelog5 <marcelovictorg2@gmail.com>
2021-09-04 10:50:36 -0300

    Adição das migrações do Cliente
    Co-authored-by: Washington Bispo <juniorbispo515@gmail.com>

---
+++ Lavanderia/DataBase/Migrations/20180430_AddClienteTable.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lavanderia.DataBase.Migrations
+{
+    [Migration(20180430121800)]
+    public class AddClienteTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Cliente")
+                .WithColumn("ClienteId").AsInt64().PrimaryKey().Identity()
+                .WithColumn("email").AsString()
+                .WithColumn("nome").AsString()
+                .WithColumn("cpf").AsString();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Cliente");
+        }
+    }
+}
--- Lavanderia/Program.cs
+++ Lavanderia/Program.cs
@@ -6,6 +6,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Initialization;
+
+using Microsoft.Extensions.DependencyInjection;
+using Lavanderia.DataBase.Migrations;
+using Lavanderia.Config.DBConfig;
 
 namespace Lavanderia
 {
@@ -13,6 +19,15 @@ namespace Lavanderia
     {
         public static void Main(string[] args)
         {
+            var serviceProvider = CreateServices();
+
+            // Put the database update into a scope to ensure
+            // that all resources will be disposed.
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -22,5 +37,39 @@ namespace Lavanderia
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        /// <summary>
+        /// Configure the dependency injection services
+        /// </summary>
+        private static IServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                // Add common FluentMigrator services
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    // Add SQLite support to FluentMigrator
+                    .AddMySql5()
+                    // Set the connection string
+                    .WithGlobalConnectionString(DBConection.Conexao)
+                    // Define the assembly containing the migrations
+                    .ScanIn(typeof(AddClienteTable).Assembly).For.Migrations())
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                // Build the service provider
+                .BuildServiceProvider(false);
+        }
+
+        /// <summary>
+        /// Update the database
+        /// </summary>
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
+
+        }
     }
 }
