1ee4fb5d5da12c101d7b1f03396f3cba09ace22e
marcelog5 <marcelovictorg2@gmail.com>
2021-09-06 15:57:07 -0300

    Cors para o fastAPI

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
-from Agenda.routers import Agenda	
+from Agenda.routers import Agenda
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[""],
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
 
 app.include_router(Agenda.router);
+
+@app.get("/")
+def main():
+    return {"message": "Hello World"}
